7bad7a858b8db5a779e5fa8911c5e1d271220515
TheGoldKay <jhonnatanwfc@live.com>
2025-02-20 06:47:20 -0300

    from profile to general search

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from linkedin_api import Linkedin
 
 api = Linkedin("email", "password")
 
-profile = api.get_profile(public_id="jhonnatan-carvalho")
+# TARGET SPECIFIC PROFILES
+# print(help(api.get_profile)) for more info on usage
 
+profile = api.get_profile(public_id="jhonnatan-carvalho")
 
 headline = profile.get('headline')
 
@@ -12,3 +14,8 @@ experience = profile.get('experience')
 languages = profile.get('languages')
 
 experience = profile.get('experience')
+
+# USE SEARCH METHOD
+# print(help(api.search_people)) for more info on usage
+
+search_results = api.search_people(keywords='python', limit=10) # cap at 10 profiles
