2c785b61a3e2f5258d02decf6c1831c0c5eb649a
TheGoldKay <jhonnatanwfc@live.com>
2025-02-25 20:17:21 -0300

    hide cred | use uv (uv pip freeze > req.txt)

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from linkedin_api import Linkedin
+import json 
+from pprint import pprint
 
-api = Linkedin("email", "password")
+with open('cred.json', 'r') as f:
+    cred = json.load(f)
+
+api = Linkedin(cred["email"], cred["password"])
 
 # TARGET SPECIFIC PROFILES
 # print(help(api.get_profile)) for more info on usage
 
-profile = api.get_profile(public_id="jhonnatan-carvalho")
+# profile = api.get_profile(public_id="jhonnatan-carvalho")
 
-headline = profile.get('headline')
+# headline = profile.get('headline')
 
-experience = profile.get('experience')
+# experience = profile.get('experience')
 
-languages = profile.get('languages')
+# languages = profile.get('languages')
 
-experience = profile.get('experience')
+# experience = profile.get('experience')
 
 # USE SEARCH METHOD
 # print(help(api.search_people)) for more info on usage
 
 search_results = api.search_people(keywords='python', limit=10) # cap at 10 profiles
+
+pprint(search_results)
